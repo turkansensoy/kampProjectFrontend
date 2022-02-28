c9a378532d007b3c23d2f925edf20e35a53ceb8a
türkan şensoy <trksnsy01@gmail.com>
2022-02-28 18:08:32 +0300

    Yazılım Geliştirici Yetiştirme Kampı

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http';  //react'da axios,fetch olarak yapılır
+import { Observable } from 'rxjs';
+import { ListResponseModel } from '../models/listResponseModel';
+import { Product } from '../models/product';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  apiUrl="https://localhost:44373/api/products/getall";
+  constructor(private httpClient:HttpClient) { }
+
+  getProducts():Observable<ListResponseModel<Product>>{
+    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  }
+}
